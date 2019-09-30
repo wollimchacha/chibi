a386493ba3ae1b2b3d2ad8a022cec01ee2ad084e
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-09-30 16:52:04 +0900

    impossible + * mixed

--- calc.py
+++ calc.py
@@ -3,7 +3,11 @@ def calc(s):
     nums = map(int,s.split('+'))
     print('nums=',nums)
     return sum(nums)
-
+    nums = map(int,s.split('*'))
+    print('nums=',nums)
+    return numpy.prod(nums)
+    
 print(calc("1"))
 print(calc("1+2"))
 print(calc("1+2+3"))
+print(calc("1*2+3"))

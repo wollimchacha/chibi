4781e7be5eca3a307a62e3681c1222f2dc63bbca
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-10-07 15:44:58 +0900

    count

---
+++ counter.py
@@ -0,0 +1,16 @@
+
+class Counter(object):
+    def__init__(self):
+        self.cnt = 0
+
+     def__init__(self):
+        self.cnt += 1
+    def__init__(self):
+        self.cnt = 0
+    def show(self):
+        print(self.cont)
+
+c = counter()
+c.show()
+c.count()
+c.show()

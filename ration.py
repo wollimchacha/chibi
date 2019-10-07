683a5ea3ad15ee2c86b14108734ff5cd4751ddad
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-10-07 16:42:39 +0900

    dhk

--- ration.py
+++ ration.py
@@ -12,6 +12,12 @@ class Q(object):
                 c = q.a
                 d = q.b
                 return Q(a*d+b*c, b*d)
+        def __sub__(self, q):
+                a = self.a
+                b = self.b
+                c = q.a
+                d = q.b
+                return Q(a*d-b*c, b*d)
 
 q1 = Q(1, 2)
 q2 = Q(1, 3)

91f97c3d1531e7307eee2e50846c7761a11e445f
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-10-07 16:16:53 +0900

    adfg

--- ration.py
+++ ration.py
@@ -2,6 +2,10 @@ class Q(object):
         def__init__(self,a,b):
             self.a = a:
             self.b = b:
+        def__repr__(self):
+            if self.b == 1:
+                    return str(self.a)
+                return f'{self.a}/{self.b}'
 
-q = Q(1, 2)
+q = Q(3)
 print(q)

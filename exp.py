00d763a4a91015d18246794347c3d29b6fa3b693
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-10-14 16:15:55 +0900

    sub mul

--- exp.py
+++ exp.py
@@ -31,6 +31,31 @@ e = Add(Val(1), Add(Val(2), Val(3)))
 print(e.eval())
 assert e.eval() == 6
 
+class Mul(object):
+    __slots__ = ['left', 'right']
+    def __init__(self, a, b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+e = Mul(Val(1), Val(2))
+print(e.eval())
+assert e.eval() == 2
+
+
+class Sub(object):
+    __slots__ = ['left', 'right']
+    def __init__(self, a, b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+e = Sub(Val(1), Val(2))
+print(e.eval())
+assert e.eval() == -1
+
 
 
 print()

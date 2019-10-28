59eef07a291e1359988b35a33dbe83c8c9b54421
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-10-28 16:11:26 +0900

    Add

--- parser.py
+++ parser.py
@@ -4,13 +4,28 @@ def parse(s: str):
     num = int(s)
     return Val(num)
 
+'''
 e = parse("123")
 print(e)
 
-s = "1+2"
+s = "123+456"
 pos = s.find('+') #+記号を探す
 print('pos', pos)
 
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s, s1, s2) #+記号で分割
+'''
+
+def parse(s: str):
+    pos = s.find('+')
+    if pos == -1:
+        num = int(s)
+        return Val(num)
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(Val(int(s1)), Val(int(s2)))
+
+e = parse("123+456")
+print(e)

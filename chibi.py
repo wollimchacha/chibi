38f9eabb839c43fa03605b6c01e71a0e151aabf8
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-12-16 16:29:25 +0900

    Scope

--- chibi.py
+++ chibi.py
@@ -120,46 +120,40 @@ class If(Expr):
         else:
             return self.else_.eval(env)
 class Lambda(Expr):
-    __slots__=['name', 'body', ]
+    __slots__ = ['name', 'body']
     def __init__(self, name, body):
-        self.name=name
-        self.body=body
-
-    def __reper__(self):
-        return f'λ{self.name}.{str(self.body)}'
+        self.name = name
+        self.body = body
+    def __repr__(self):
+        return f'λ{self.name} . {str(self.body)}'
     def eval(self, env):
-        pass
-
-f = Lambda('x', Add(Var('x'), 1))   #λx . x+1
-print(reper(f))
-
-def copy(env)
+        return self
+def copy(env): #環境をコピーすることでローカルスコープを作る
     newenv = {}
-    for x in env.keys:
-        newenv[x]=env[x]
+    for x in env.keys():
+        newenv[x] = env[x]
     return env
-
 class FuncApp(Expr):
-    __slots__=['func', 'param']
-    def__init__(self, func: Lambda, param):
-        self.func=func
-        self.param=Expr.new(param)
-    def __reper__(self):
-        return f'({reper(self.func)}) ({reper(self.param)})'
+    __slots__ = ['func', 'param']
+    def __init__(self, func: Lambda, param):
+        self.func = func
+        self.param = Expr.new(param)
+    def __repr__(self):
+        return f'({repr(self.func)}) ({repr(self.param)})'
     def eval(self, env):
-        v = self.param.eval(env)
-        name = self.func.name
-        env[name] = v
-        return self.func.body.eval(env)
-
-e = FuncApp(f, Add(1, 1))
-
-print(e, '=>', e.eval({}))
-
-
+        f = self.func.eval(env)
+        v = self.param.eval(env)  # パラメータを先に評価する
+        name = f.name # Lambda の変数名をとる
+        env = copy(env)  # 環境をコピーすることでローカルスコープを作る
+        env[name] = v   # 環境から引数を渡す
+        return f.body.eval(env)
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
+    if tree == 'FuncDecl':   # この２行を追加します
+        return Assign(str(tree[0]), Lambda(str(tree[1]), conv(tree[2])))
+    if tree == 'FuncApp':   # この２行を追加します
+        return FuncApp(conv(tree[0]), conv(tree[1]))
     if tree == 'If':
         return If(conv(tree[0]), conv(tree[1]), conv(tree[2]))
     if tree == 'While':
@@ -169,7 +163,7 @@ def conv(tree):
     if tree == 'Add':
         return Add(conv(tree[0]), conv(tree[1]))
     if tree == 'Sub':
-        return Sub(conv(tree[0]), conv(tree[1]))    
+        return Sub(conv(tree[0]), conv(tree[1]))
     if tree == 'Mul':
         return Mul(conv(tree[0]), conv(tree[1]))
     if tree == 'Div':

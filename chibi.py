fc1eb8b41ac7b3346e5c929722d56eb826116e9f
ISHIZUKA Rika <21716008@jwu.ac.jp>
2019-12-16 16:11:35 +0900

    funcapp dekinai

--- chibi.py
+++ chibi.py
@@ -119,7 +119,7 @@ class If(Expr):
             return self.then.eval(env)
         else:
             return self.else_.eval(env)
-class lambda(Expr):
+class Lambda(Expr):
     __slots__=['name', 'body', ]
     def __init__(self, name, body):
         self.name=name
@@ -127,16 +127,34 @@ class lambda(Expr):
 
     def __reper__(self):
         return f'λ{self.name}.{str(self.body)}'
-    def eval(self, env)
+    def eval(self, env):
         pass
 
 f = Lambda('x', Add(Var('x'), 1))   #λx . x+1
 print(reper(f))
 
+def copy(env)
+    newenv = {}
+    for x in env.keys:
+        newenv[x]=env[x]
+    return env
+
 class FuncApp(Expr):
     __slots__=['func', 'param']
-    def__init__(self, func:Lambda, param):
+    def__init__(self, func: Lambda, param):
+        self.func=func
+        self.param=Expr.new(param)
+    def __reper__(self):
+        return f'({reper(self.func)}) ({reper(self.param)})'
+    def eval(self, env):
+        v = self.param.eval(env)
+        name = self.func.name
+        env[name] = v
+        return self.func.body.eval(env)
 
+e = FuncApp(f, Add(1, 1))
+
+print(e, '=>', e.eval({}))
 
 
 def conv(tree):
